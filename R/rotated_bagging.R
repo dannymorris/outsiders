6646231bc4519baab2201dda3f02ad3871a19207
dannymorris <usc00@hotmail.com>
2018-06-28 21:19:08 -0400

    updates

--- R/rotated_bagging.R
+++ R/rotated_bagging.R
@@ -6,59 +6,57 @@
 #' @export
 #' 
 pca_bag <- function(data, n_features = floor(sqrt(ncol(data))),
-                    n_iterations=50, method) {
+                    n_iterations = 50, outlier_fun, scores_fun = sum,
+                    scores_only = TRUE) {
     
     # PCA rotated feature bagging is an ensemble-centric, unsupervised outlier detection
     # technique for locating outliers in feature subspaces in high dimensional data. The
     # algorithm is provided a defined number of iterations, a random subset of features
-    # is obtained, rotated via principal components analysis and retaining all components, 
+    # is obtained, rotated via principal components analysis with all components retained, 
     # and final outlier scores are combined by summing scores from individual iterations.
     
     # Subspace outlier detection methods work by locating outliers in locally relevant
     # feature sets in high dimensional data.
     
-    if (missing(method)) {
+    if (missing(outlier_fun)) {
         stop("A base detector method must be specified. For example, try
              method = function(x) mahalanobis(x, center=colMeans(x), cov=cov(x))
              for computing the Mahalanobis distances in each iteration")
     }
     
     if (missing(n_iterations)) {
-        message("Message: Number of iterations defaults to 50")
+        warning("n_iterations not specified. Defaults to 50")
     }
     
     if (missing(n_features)) {
-        message("Message: Number of features in each subsample defaults to the
-                square root of the number of columns rounded down to the nearest integer")
+        warnings("n_features not specified. Defaults to the square root of the number of 
+                  original features rounded down to the nearest integer")
     }
     
-    data_df <- make_tibble(data)
+    data_tbl <- make_tibble(data)
     
-    score_matrix <- matrix(nrow = nrow(data_df), ncol = n_iterations)
+    score_matrix <- matrix(nrow = nrow(data_tbl), 
+                           ncol = n_iterations)
     
     # iterate over the features m times and subset r subsets of features
     # rotate each subsample with PCA, retaining all components
     # score the data points with a method applied to pca rotated subsample
     # insert subsamples scores into score matrix
     for (i in 1:n_iterations) {
-        features_index <- sample(1:ncol(data_df), n_features, F)
-        data_subset <- data_df[, features_index]
-        data_subset_pca <- princomp(data_subset)$scores
-        score <- method(data_subset_pca)
-        score_matrix[, i] <- score
+        features_index <- sample(1:ncol(data_tbl), n_features, F)
+        data_subset <- data_tbl[, features_index]
+        pca_scores <- princomp(data_subset)$scores
+        outlier_score <- do.call(method, list(pca_scores))
+        score_matrix[, i] <- outlier_score
     }
     
-    # if (scale == TRUE) {
-    #     score_matrix <- scale(score_matrix)
-    # } else {
-    #     score_matrix
-    # }
+    scores <- apply(score_matrix, 1, scores_fun, na.rm = T)
     
-    score_matrix_scaled <- scale(score_matrix)
-    
-    combined_errors <- apply(score_matrix_scaled, 1, sum, na.rm = T)
-    
-    #return(combined_errors)
-    return(score_matrix_scaled)
-    
+    if (scores_only == TRUE) {
+        return(scores)
+    } else {
+        output_ls <- list(score_matrix = score_matrix, 
+                          scores = scores)
+        return(output_ls)
+    }
 }
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # outsiders
 
-`outsiders` is an R package for outlier analysis and anomaly detection. Most of the current functions are intended for use with multidimensional data sets.
+`outsiders` is an R package for multidimensional outlier analysis and anomaly detection. Many of the techniques available in this package are inspired by *Outlier Analysis* (C. C. Aggarwal. Springer, 2017.). 
+
+# Installation
+
+`devtools::install_github("dannymorris/outsiders")` for quick and easy installation
+
+# Featured Methods
 
 ## `ALSO()`
 
-Attribute-wise Learning for Scores Outliers (ALSO) is an unsupervised outlier detection technique for locating outliers among a set of correlated numeric observations. The technique assumes that correlated features can be used to predict one another, and data points are considered outliers if they deviate significantly from the correlation structure. The predictability of a feature determines its weight in the scoring process. Features that can be predicted with high accuracy are weighted more heavily than unpredictable features. Weights are defined as 1 - min(1, RMSE). If the root mean squared error (RMSE) of a feature model exceeds 1, the weight of the dependent feature defaults to 0 since the RMSE of a predictive model which only predicts the mean of the dependent variable for all data points is always 1. Otherwise, the feature weight is the RMSE of its own model. Final scoring is accomplished by summing the weighed scores. Extreme-value analysis can then be applied to locate outliers that deviate from the correlation structure of the data set. 
+Attribute-wise Learning for Scores Outliers (ALSO) combines supervised and unsupervised outlier detection to locate outliers among a set of correlated features. The technique assumes correlated features can be used to predict one another, and data points are considered outliers if they deviate significantly from the correlation structure. The predictability of a feature determines its weight in the scoring process. Features that can be predicted with high accuracy are weighted more heavily than unpredictable features. Weights are defined as 1 - min(1, RMSE). If the root mean squared error (RMSE) of a feature model exceeds 1, the weight of the dependent feature defaults to 0 since the RMSE of a predictive model which only predicts the mean of the dependent variable for all data points is always 1. Otherwise, the feature weight is the RMSE of its own model. Final scoring is accomplished by summing the weighed scores. Extreme-value analysis can then be applied to locate outliers that deviate from the correlation structure of the data set. 
+
+**Example Usage**
+
+`outsiders::also(data = state.x77, scale_numerics = TRUE, method = lm, cross_validate = TRUE, n_folds = 10, scores_only = TRUE)`
